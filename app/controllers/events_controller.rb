49295fd2bac7271e641d560aa86e72ae0cf06bc1
katherine95 <kathiekim95@gmail.com>
2018-04-23 12:09:05 +0300

    (feature)create an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,64 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:edit, :update, :show, :delete]
+
+  # Index action to render all events
+  def index
+    @events = Event.all
+  end
+
+  # New action for creating event
+  def new
+    @event = Event.new
+  end
+
+  # Create action saves the event into database
+  def create
+    @event = Event.new
+    if @event.save(event_params)
+      flash[:notice] = "Successfully created event!"
+      redirect_to event_path(@event)
+    else
+      flash[:alert] = "Error creating new event!"
+      render :new
+    end
+  end
+
+  # Edit action retrives the event and renders the edit page
+  def edit
+  end
+
+  # Update action updates the event with the new information
+  def update
+    if @event.update_attributes(event_params)
+      flash[:notice] = "Successfully updated event!"
+      redirect_to event_path(@events)
+    else
+      flash[:alert] = "Error updating event!"
+      render :edit
+    end
+  end
+
+  # The show action renders the individual event after retrieving the the id
+  def show
+  end
+
+  # The destroy action removes the event permanently from the database
+  def destroy
+    if @event.destroy
+      flash[:notice] = "Successfully deleted event!"
+      redirect_to events_path
+    else
+      flash[:alert] = "Error updating event!"
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
 end
