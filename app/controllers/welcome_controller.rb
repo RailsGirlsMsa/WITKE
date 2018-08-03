1f6af06087c43f891cb1b2c025e0e220a60e616e
imahnama <muannama25@gmail.com>
2018-08-03 18:18:03 +0300

    modified files and deleted media

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def WOF
+  def tech
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+
+  get 'welcome/tech'
+
+  root 'welcome#tech'
 end
